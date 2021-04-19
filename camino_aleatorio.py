e88880ee8c9bd002e4e440d0fad1215cfa02b28c
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-18 20:42:51 -0400

    grafico

--- camino_aleatorio.py
+++ camino_aleatorio.py
@@ -2,43 +2,44 @@ from borracho import BorrachoTradicional
 from campo import Campo
 from coordenada import Coordenada
 
-def caminata(campo, borracho, pasos):
-    inicio = campo.obtener_coordenada(borracho)
+from bokeh.plotting import figure, show
+
+def caminata(campo, pasos, tipo_de_borracho):
+    borracho = tipo_de_borracho(nombre='Steven')
+    origen = Coordenada(0, 0)
+    campo.anadir_borracho(borracho, origen)
+
+    pasos_x = []
+    pasos_y = []
+
+    pasos_x.append(origen.x)
+    pasos_y.append(origen.y)
 
     for _ in range(pasos):
         campo.mover_borracho(borracho)
-
-    return inicio.distancia(campo.obtener_coordenada(borracho))
-
-def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
-    borracho = tipo_de_borracho(nombre = 'Davidad')
-    origen = Coordenada(0, 0)
-    distancias = []
-
-    for _ in range(numero_de_intentos):
-        campo = Campo()
-        campo.anadir_borracho(borracho, origen)
-        simulacion_caminata = caminata(campo, borracho, pasos)
-        distancias.append(round(simulacion_caminata, 1))
-
-    return distancias
-
-def main(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
-    
-    for pasos in distancias_de_caminata:
-        distancias = simular_caminata(pasos, numero_de_intentos, tipo_de_borracho)
-
-        distancia_media = round(sum(distancias) / len(distancias), 4)
-        distancia_maxima = max(distancias)
-        distancia_minima = min(distancias)
-        print(f'{tipo_de_borracho.__name__} caminata aleatoria de {pasos}')
-        print(f'Media = {distancia_media}')
-        print(f'Max = {distancia_maxima}')
-        print(f'Min = {distancia_minima}')
+        
+        pasos_x.append(campo.obtener_coordenada(borracho).x)
+        pasos_y.append(campo.obtener_coordenada(borracho).y)
+    #print(f'x {pasos_x}, y {pasos_y}')
+        
+    return (pasos_x, pasos_y)
 
 
-if __name__ ==  '__main__':
-    distancias_de_caminata = [10, 100, 1000, 10000]
-    numero_de_intentos = 100
+def graficar(x, y,pasos):
+    grafica = figure(title='Random Walks',x_axis_label='x axis', y_axis_label='y axis')
+    grafica.line(x, y, legend_label='walk', color='skyblue',name='steven') #recorrido
+    grafica.line(x[0:-1:pasos-1],y[0:-1:pasos-1], color='black', line_width=3) #linea de punto inicial a punto final
+    print(f'x {x[0:-1:pasos-1]}, y {y[0:-1:pasos-1]}')
+    show(grafica)
 
-    main(distancias_de_caminata, numero_de_intentos, BorrachoTradicional)
+def main(pasos, tipo_de_borracho):
+    campo=Campo()
+    pasos_x, pasos_y = caminata(campo, pasos, tipo_de_borracho)
+    #print(f'x {pasos_x} y {pasos_y}')
+    graficar(pasos_x, pasos_y, pasos)
+
+
+if __name__ == '__main__':
+
+    pasos = 3000
+    main(pasos, BorrachoTradicional)
